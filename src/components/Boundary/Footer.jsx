06daedd94434a919d05af33063f0f80d1dcf2930
Bhavesh Bhanusali <bhaveshbhanusalip884@gmail.com>
2024-02-01 20:59:54 +0530

    tweeking done

--- src/components/Boundary/Footer.jsx
+++ src/components/Boundary/Footer.jsx
@@ -3,26 +3,26 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="relative w-full h-auto bg-[#F2F2F2] mt-[3vw] px-[8vw] py-[2vw] ">
-      <div className="flex text-[#C5C3C3] text-[8vw] w-full font-bold text-center justify-between  items-center ">
+    <div className="relative w-full h-auto bg-[#F2F2F2] mt-[3vw] px-[8vw] py-[3vw] sm:py-[2vw] ">
+      <div className="flex text-[#C5C3C3] text-[9vw] sm:text-[8vw] w-full font-bold text-center justify-between  items-center ">
         UltraXpert
-        <span className="text-black text-[1.4vw] text-right">
+        <span className="text-black text-[2vw] sm:text-[1.8vw] md:text-[1.4vw] text-right">
           Think Creative, Do Effective
         </span>
       </div>
       <div className="flex flex-col md:flex-row mt-[1.8vw] md:justify-between">
-        <div className="w-11/12 md:w-1/2 mb-[2vw] md:mb-0  md:px-4 font-medium text-[1.8vw] md:text-[1.2vw] text-justify">
+        <div className="w-11/12 md:w-1/2 mb-[2vw] md:mb-0  md:px-4 font-medium text-[2.18vw] sm:text-[1.8vw] md:text-[1.2vw] text-justify">
           At UltraCreation, our mission is to transcend conventional boundaries
           and empower businesses with cutting-edge technological solutions. We
           are dedicated to being the catalyst for our clients' digital
           transformation, facilitating seamless connectivity, and maximizing
           their potential in the ever-evolving digital landscape.
-          <div className="mt-[1.4vw] font-bold text-[2.2vw]">
+          <div className="mt-[1.8vw] sm:mt-[1.4vw] font-bold text-[2.65vw] sm:text-[2.2vw]">
             Helping Millions, Grow Better
           </div>
         </div>
-        <div className=" w-2/3 md:w-1/2 flex justify-between md:justify-normal md:gap-8">
-          <div className="w-1/3 list-none flex flex-col gap-[1.4vw] font-medium text-[1.8vw] md:text-[1.2vw] ml-[-2vw] md:ml-[6vw] overflow-hidden">
+        <div className=" w-2/3 md:w-1/2 flex justify-between md:justify-normal md:gap-8 mt-[1.2vw] sm:mt-[0vw]">
+          <div className="w-1/3 list-none flex flex-col gap-[2.4vw] sm:gap-[1.4vw] font-medium text-[2.5vw] sm:text-[1.8vw] md:text-[1.2vw] ml-[-2vw] md:ml-[6vw] overflow-hidden">
             <li className="underline underline-offset-[3px]  pl-5 hover:scale-105">
               Home
             </li>
@@ -39,7 +39,7 @@ export const Footer = () => {
               Service
             </li>
           </div>
-          <div className="w-2/3 ml-[-6vw] gap-[1.05vw] flex flex-col justify-between text-[1.8vw] md:text-[1.2vw]">
+          <div className="w-2/3 ml-[-6vw] gap-[1.85vw] sm:gap-[1.05vw] flex flex-col justify-between text-[2.5vw] sm:text-[1.8vw] md:text-[1.2vw]">
             <div className="w-full border-t-2 border-[#d4d4d4] border-solid pt-[0.6vw]">
               <b>Email:</b> <br /> Bhaveshbhanusali1@gmail.com
             </div>
