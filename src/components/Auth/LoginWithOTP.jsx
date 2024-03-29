f54f9a9fb92b025ea55d84fd3ece1f10cd7df754
Bhavesh Bhanusali <bhaveshbhanusalip884@gmail.com>
2024-03-29 14:11:33 +0530

    temp sign up

--- src/components/Auth/LoginWithOTP.jsx
+++ src/components/Auth/LoginWithOTP.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import userImage from "../../assets/images/image.png";
 import { useNavigate } from "react-router-dom";
+import axios from "../../axios";
 
 const LoginWithOTP = () => {
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const [showOtp, setShowOtp] = useState(false);
   const navigate = useNavigate();
 
   const handleGoogleLink = () => {
@@ -13,9 +17,15 @@ const LoginWithOTP = () => {
     navigate("/login");
   };
 
-  const handleSentOTP = () => {
-    alert("OTP sent successfully to your mobile number");
-    navigate("/sentOTP");
+  const handleSentOTP = async (e) => {
+    e.preventDefault();
+    setShowOtp(true);
+    const response = await axios.get(
+      `/verify/?action=2&email=${email}&otp=${otp}`
+    );
+    console.log(response);
+    alert("OTP sent successfully to your email address");
+    // navigate("/sentOTP");
   };
 
   const handleSignUp = () => {
@@ -26,43 +36,57 @@ const LoginWithOTP = () => {
     <div className="h-full mt-[100px] bg-white m-auto flex justify-center items-center">
       <div className="lg:max-w-[50vw] md:w-[75%] w-[90%] flex md:flex-row flex-col bg-white px-8 pb-8 rounded-xl shadow-md border border-solid border-[#a3a3a3]">
         <div className="flex flex-col md:w-[50%] w-full">
-          <h1 className="text-4xl font-bold mb-8">Login With OTP</h1>
-          <label htmlFor="mobileNumber" className="text-lg font-semibold mb-2">
-            Enter your mobile number:
-          </label>
-          <input
-            type="text"
-            id="mobileNumber"
-            placeholder="Enter your mobile number"
-            className="border rounded-md mb-4 p-2 w-full"
-          />
-          <div className="flex justify-between">
-            <p
-              onClick={handleLogin}
-              className="text-xs cursor-pointer text-[#272727] underline hover:text-blue-500 cursor pointer"
+          <form method="post">
+            <h1 className="text-4xl font-bold mb-8">Login With OTP</h1>
+            <label htmlFor="email" className="text-lg font-semibold mb-2">
+              Enter your email address:
+            </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              placeholder="Enter your email address"
+              className="border rounded-md mb-4 p-2 w-full"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {showOtp && (
+              <input
+                type="number"
+                value={otp}
+                id="OTP"
+                onChange={(e) => setOtp(e.target.value)}
+                placeholder="Enter your OTP here"
+                className="border rounded-md mb-4 p-2 w-full"
+              />
+            )}
+            <div className="flex justify-between">
+              <p
+                onClick={handleLogin}
+                className="text-xs cursor-pointer text-[#272727] underline hover:text-blue-500 cursor pointer"
+              >
+                Login with Email & Password?
+              </p>
+              <p
+                onClick={handleGoogleLink}
+                className="text-xs text-[#272727] hover:text-blue-500 cursor pointer underline"
+              >
+                Login with Google?
+              </p>
+            </div>
+            <button
+              onClick={handleSentOTP}
+              className="bg-[#272727] text-lg text-white cursor-pointer font-semibold mb-[1vw] py-2 px-4 rounded-md w-full"
             >
-              Login with Email?
-            </p>
-            <p
-              onClick={handleGoogleLink}
-              className="text-xs text-[#272727] hover:text-blue-500 cursor pointer underline"
+              Send OTP
+            </button>
+            <p className="text-xs underline">Want to create an account?</p>
+            <button
+              onClick={handleSignUp}
+              className="bg-white text-[#272727] w-full text-lg font-semibold px-4 py-2 cursor-pointer rounded-md border border-solid border-[#272727]"
             >
-              Login with Google?
-            </p>
-          </div>
-          <button
-            onClick={handleSentOTP}
-            className="bg-[#272727] text-lg text-white cursor-pointer font-semibold mb-[1vw] py-2 px-4 rounded-md w-full"
-          >
-            Send OTP
-          </button>
-          <p className="text-xs underline">Want to create an account?</p>
-          <button
-            onClick={handleSignUp}
-            className="bg-white text-[#272727] w-full text-lg font-semibold px-4 py-2 cursor-pointer rounded-md border border-solid border-[#272727]"
-          >
-            Sign Up
-          </button>
+              Sign Up
+            </button>
+          </form>
         </div>
         <div className="md:w-[50%] flex items-center justify-center">
           <img
